ee5a6d442420dfc71c9be64889467f9c5ffc8052
Dygenlow <edmundsoo36@gmail.com>
2020-11-06 18:35:20 +0800

    Update 1.0

---
+++ Assets/TestScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestScript : MonoBehaviour
+{
+    public GameObject scoreText;
+
+    float spaceBarPressed = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            scoreText.GetComponent<Text>().text = ("SpaceBar: " + spaceBarPressed);
+
+            print("SpaceBar Pressed: " + spaceBarPressed);
+
+            ++spaceBarPressed;
+        }
+    }
+}
